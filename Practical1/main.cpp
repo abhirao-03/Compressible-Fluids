21621cf7106f7df1827a4d905236b42eeb4e2761
s2230095 <s2230095@ed.ac.uk>
2025-11-03 16:04:20 +0000

    Completed Practical 1

--- Practical1/main.cpp
+++ Practical1/main.cpp
@@ -4,14 +4,18 @@
 int main(){
     double X0 = 0.0;
     double X1 = 1.0;
-    double T0 = 0.0;
-    double TEND = 1.0;
+    
     double ADVCOEFF = 1.0;
-    int NUMPOINTS = 100;
+    
+    double TEND = 1.0;
+    double T0 = 0.0;
+    
+    int NUMPOINTS = 400;
     int NUMGHOST = 2;
     int INITCOND;
-    Simulation::DifferenceMethod DIFFMETHOD = Simulation::DifferenceMethod::CENTRAL;
-    double RELAXATION = 1.0;
+
+    Simulation::DifferenceMethod DIFFMETHOD = Simulation::DifferenceMethod::BACKWARD;
+    double RELAXATION = 0.9;
 
     std::cin >> INITCOND;
 
