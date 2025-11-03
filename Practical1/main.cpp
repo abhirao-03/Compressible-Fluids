3b1682def4ca2092855a380305e0cb2fc125137d
s2230095 <s2230095@ed.ac.uk>
2025-11-03 10:36:21 +0000

    changed folder name

---
+++ Practical1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "simulation.h"
+
+int main(){
+    double X0 = 0.0;
+    double X1 = 1.0;
+    double T0 = 0.0;
+    double TEND = 1.0;
+    double ADVCOEFF = 1.0;
+    int NUMPOINTS = 100;
+    int NUMGHOST = 2;
+    int INITCOND;
+    int DIFFMETHOD = 1;
+    double RELAXATION = 1.0;
+
+    std::cin >> INITCOND;
+
+    Simulation DownwindSin(X0, X1, T0, TEND, ADVCOEFF, NUMPOINTS, NUMGHOST, INITCOND, DIFFMETHOD, RELAXATION);
+    DownwindSin.PerformTimeSteps();
+
+    return 0;
+}
