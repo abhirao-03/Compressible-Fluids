407861fbbbc929150ef54a92bc2c057005aeeab0
s2230095 <s2230095@ed.ac.uk>
2025-11-12 13:25:27 +0000

    working on LaxFriedrichs flux

--- fluxes.cpp
+++ fluxes.cpp
@@ -1,7 +1,19 @@
 #include "simulation.h"
+#include <cmath>
 
 void Simulation::m_fvm_LaxFriedrichs(const std::vector<double>& vec_dOldU, std::vector<double>& vec_dNewU, const int& i_IndexUpdate)
 {
+    double uL = vec_dOldU[i_IndexUpdate];
+    double uR = vec_dOldU[i_IndexUpdate + 1];
+
+    double fL = 0.5*pow(uL, 2.0);
+    double fR = 0.5*pow(uR, 2.0);
+
+    double LaxFlux_One = (1.0/2.0) * (fL + fR);
+
+    // need to do the above computation ONE MORE TIME and then pass LaxFlux_two
+    // this is draft code, this will be changed relatively soon.
+    vec_dNewU[i] = uL + m_dDeltaT / m_dDeltaX * (LaxFlux_One);
 
 }
 
