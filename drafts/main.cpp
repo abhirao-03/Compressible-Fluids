1937da0394d3342d7f181df2c3ecc640d7640c71
Abhijeet Rao <abhijeetraop@gmail.com>
2025-11-17 17:48:28 +0000

    fixed flux issue

--- drafts/main.cpp
+++ drafts/main.cpp
@@ -18,62 +18,68 @@ int main()
     double u_T0 = 0.0;
     
     int u_NUMPOINTS = 100;
-    int u_NUMGHOST = 4;
+    int u_NUMGHOST = 2;
     double u_RELAXATION = 0.8;
     
     double d_X = (u_X1 - u_X0 )/u_NUMPOINTS;
     double d_T = u_RELAXATION * d_X;
 
+    
     std::vector<double> vec_dU;
-    vec_dU.resize(u_NUMGHOST + u_NUMPOINTS + 1);
+    vec_dU.resize(u_NUMGHOST + u_NUMPOINTS);
+
+    std::vector<double> fluxes;
+    fluxes.resize(vec_dU.size());
 
     std::vector<double> new_vec_dU;
-    new_vec_dU.resize(u_NUMGHOST + u_NUMPOINTS + 1);
+    new_vec_dU.resize(vec_dU.size());
 
     for(int i = 0; i < vec_dU.size(); i++)
-            {
-                double x = u_X0 + (i - 0.5) * d_X;
-                
-                if (x < 0.5)
-                    {
-                        vec_dU[i] = 2;
-                    }
-                else 
-                    {
-                        vec_dU[i] = 1;   
-                    }
-                
-                // std::cout << x << ' ' << vec_dU[i] << std::endl;
-            }
-    
+        {
+            double x = u_X0 + (i - 0.5) * d_X;
+            
+            if (x < 0.5)
+                {
+                    vec_dU[i] = 2;
+                }
+            else 
+                {
+                    vec_dU[i] = 1;   
+                }
+            
+            // std::cout << x << ' ' << vec_dU[i] << std::endl;
+        }
+
+
     double t = u_T0;
     
     do
         {
             t += d_T;
 
-            vec_dU[0] = vec_dU[u_NUMPOINTS + u_NUMGHOST - 1];
-            vec_dU[u_NUMPOINTS + u_NUMGHOST] = vec_dU[1];
+            vec_dU[0] = vec_dU[u_NUMPOINTS];
+            vec_dU[u_NUMPOINTS + 1] = vec_dU[1];
+
+            for (int i = 0; i < vec_dU.size() - 1; i++)
+                {
+                    double uL = vec_dU[i];
+                    double uR = vec_dU[i + 1];
+
+                    fluxes[i] = 0.5 * (d_X/d_T) * (vec_dU[i] - vec_dU[i+1]) + 0.5 * (fluxfunction(vec_dU[i]) + fluxfunction(vec_dU[i+1]));
+                }
+            
 
             std::cout << "# time = " << t << std::endl;
 
-            for (int i = 0; i < vec_dU.size() - u_NUMGHOST/2; i++)
-            {
-                double x = u_X0 + (i - 0.5) * d_X;
+            for (int i = 1; i < vec_dU.size() - u_NUMGHOST/2; i++)
+                {
+                    double x = u_X0 + (i - 0.5) * d_X;
 
-                double uL = vec_dU[i];
-                double uR = vec_dU[i+1];
-                double fL = fluxfunction(uL);
-                double fR = fluxfunction(uR);
+                    new_vec_dU[i] = vec_dU[i] - d_T / d_X * (fluxes[i] - fluxes[i-1]);
 
-                double LF_flux_left = 0.5 * (d_X/d_T) * (vec_dU[i] - vec_dU[i+1]) + 0.5 * (fluxfunction(vec_dU[i]) + fluxfunction(vec_dU[i+1]));
-                double LF_flux_right = 0.5 * (d_X/d_T) * (vec_dU[i+1] - vec_dU[i+2]) + 0.5 * (fluxfunction(vec_dU[i+1]) + fluxfunction(vec_dU[i+2]));
+                    std::cout << x << ' ' << new_vec_dU[i] << std::endl;
 
-                new_vec_dU[i] = uL - d_T / d_X * (LF_flux_left - LF_flux_right);
-
-                std::cout << x << ' ' << new_vec_dU[i] << std::endl;
-
-            }
+                }
 
             std::cout << "\n\n";
 
