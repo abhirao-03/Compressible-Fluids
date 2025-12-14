9dd8e30efe6f95e460ccc6c584a8b1d7685e6f43
s2230095 <s2230095@ed.ac.uk>
2025-12-14 19:13:52 +0000

    Added functionality to handle euler conserved variables

--- fluxes.cpp
+++ fluxes.cpp
@@ -5,11 +5,11 @@ void Simulation::m_fvm_LaxFriedrichs()
     {
         for (int i = 0; i < m_vec_dU.size() - 1; i++)
             {
-                double t_dULeft = m_vec_dU[i];
-                double t_dURight = m_vec_dU[i + 1];
+                vec3 t_dULeft = m_vec_dU[i];
+                vec3 t_dURight = m_vec_dU[i + 1];
 
-                double t_dFluxLeft = m_BurgersFluxFunction(t_dULeft);
-                double t_dFluxRight = m_BurgersFluxFunction(t_dURight);
+                vec3 t_dFluxLeft = m_EulerFluxFunction(t_dULeft);
+                vec3 t_dFluxRight = m_EulerFluxFunction(t_dURight);
 
                 m_vec_dFluxes[i] = 0.5 * (t_dFluxLeft + t_dFluxRight) + 0.5 * (m_dDeltaX/m_dDeltaT) * (t_dULeft - t_dURight);
             }
@@ -17,75 +17,32 @@ void Simulation::m_fvm_LaxFriedrichs()
 
 void Simulation::m_fvm_Richtmyer()
     {
-        double u_halfs;
+        vec3 u_halfs;
 
         for (int i = 0; i < m_vec_dFluxes.size() - 1; i++)
             {
-                double u_left = m_vec_dU[i];
-                double u_right = m_vec_dU[i + 1];
+                vec3 u_left = m_vec_dU[i];
+                vec3 u_right = m_vec_dU[i + 1];
 
-                double u_FluxLeft = m_BurgersFluxFunction(u_left);
-                double u_FluxRight = m_BurgersFluxFunction(u_right);
+                vec3 u_FluxLeft = m_EulerFluxFunction(u_left);
+                vec3 u_FluxRight = m_EulerFluxFunction(u_right);
 
                 u_halfs = 0.5 * (u_left + u_right) - 0.5 * (m_dDeltaT / m_dDeltaX) * (u_FluxRight - u_FluxLeft);
 
-                m_vec_dFluxes[i] = m_BurgersFluxFunction(u_halfs);
+                m_vec_dFluxes[i] = m_EulerFluxFunction(u_halfs);
             }   
     }
 
 void Simulation::m_fvm_FORCE()
     {
         m_fvm_LaxFriedrichs();
-        std::vector<double> t_vec_LFFluxes = m_vec_dFluxes;
+        std::vector<vec3> t_vec_LFFluxes = m_vec_dFluxes;
 
         m_fvm_Richtmyer();
-        std::vector<double> t_vec_RichtmyerFluxes = m_vec_dFluxes;
+        std::vector<vec3> t_vec_RichtmyerFluxes = m_vec_dFluxes;
 
         for (int i = 0; i < m_vec_dFluxes.size() - 1; i++)
             {
                 m_vec_dFluxes[i] = 0.5 * (t_vec_LFFluxes[i] + t_vec_RichtmyerFluxes[i]);
-            }
-    }
-
-void Simulation::m_fvm_Godunov()
-    {
-        for (int i = 0; i < m_vec_dU.size() - 1; i++)
-            {
-                double t_dSelectedU;
-                double t_dCurrentU = m_vec_dU[i];
-                double t_dNextU = m_vec_dU[i + 1];
-
-                if (t_dCurrentU > t_dNextU)
-                    {
-                        double t_dShockSpeed = 0.5 * (m_vec_dU[i] + m_vec_dU[i + 1]);
-
-                        if (t_dShockSpeed > 0)
-                            {
-                                t_dSelectedU = t_dCurrentU;
-
-                            }
-                        else
-                            {
-                                t_dSelectedU = t_dNextU;
-                            }
-                    }
-                    
-                else 
-                    {
-                        if (t_dCurrentU > 0)
-                            {  
-                                t_dSelectedU = t_dCurrentU;
-                            }
-                        else if (t_dCurrentU <= 0 && 0 <= t_dNextU)
-                            {
-                                t_dSelectedU = 0;
-                            }
-                        else if (t_dNextU <= 0)
-                            {
-                                t_dSelectedU = t_dNextU;
-                            }
-                    }
-                
-            m_vec_dFluxes[i] = m_BurgersFluxFunction(t_dSelectedU);
             }
     }
